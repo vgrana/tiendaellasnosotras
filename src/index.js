60e192d727e42cdd1f0b9ef5c1e4e96142fdaa9c
vgrana <valeriagrandi@hotmail.com>
2021-03-29 15:54:35 -0300

    probando deploy

--- src/index.js
+++ src/index.js
@@ -1,55 +1,27 @@
 
 express = require("express");
-bodyParser = require("body-parser");
-var cors = require("cors");
-var server = express();
+const server = express();
 const path = require("path");
-morgan = require("morgan");
+// morgan = require("morgan");
 
 
-server.use(express.json());
-server.use(cors());
-server.use(morgan("dev"));
+server.set('port', 9000);
+server.set('views',path.join(__dirname, 'views'))
+
 server.use(express.static(path.join(__dirname, 'public')));
-server.set('port', 10000);
 
-// server.set('view engine', 'html');
 
-// //routes
-// // server.use(require('./routes/index'));
-// server.get('/', (req,res) =>{
-//     res.sendFile(path.join(__dirname , 'index.html'));
-    
-// });
 
-// // server.get('/img1', (req,res) =>{
-// //     res.sendFile(path.join(__dirname ,'img1.jpg'));
+//routes
+// server.use(require('./routes/index'));
+server.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname , 'views/index.html'));
     
+  
     
-// // });
-// // server.get('/img2', (req,res) =>{
-// //     res.sendFile(path.join(__dirname ,'img2.jpg'));
-    
-// // });
-// // server.get('/img3', (req,res) =>{
-// //     res.sendFile(path.join(__dirname ,'img3.jpg'));
-    
-// // });
-// // server.get('/img4', (req,res) =>{
-// //     res.sendFile(path.join(__dirname ,'img4.jpg'));
-    
-// // });
+});
 
-// // server.get('/face', (req,res) =>{
-// //     res.sendFile(path.join(__dirname ,'face.jpg'));
-    
-// // });
-// // server.get('/instagram', (req,res) =>{
-// //     res.sendFile(path.join(__dirname ,'instagram.jpg'));
-    
-// // });
 
 server.listen(server.get('port'), () => {
     console.log("Server running on port",server.get('port'));
   });
-    
