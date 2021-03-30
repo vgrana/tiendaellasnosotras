503930d097677f767ddf8be7c33f040b394931a3
vgrana <valeriagrandi@hotmail.com>
2021-03-30 15:16:17 -0300

    comenzando css

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
-
 express = require("express");
 const server = express();
 const path = require("path");
-// morgan = require("morgan");
-
 
 server.set('port', 10000);
 server.set('views',path.join(__dirname, 'views'))
 
 server.use(express.static(path.join(__dirname, 'views')));
 
-
-
-//routes
-// server.use(require('./routes/index'));
 server.get('/', (req,res) =>{
-    res.sendFile(path.join(__dirname , 'views/index.html'));
-    
-  
-    
+    res.sendFile(path.join(__dirname , 'views/index.html'));    
 });
-
 
 server.listen(server.get('port'), () => {
     console.log("Server running on port",server.get('port'));
